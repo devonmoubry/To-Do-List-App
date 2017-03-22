bf91a61357691f560aca9af051d236b37e3d63d0
Devon Moubry <devon@moubry.com>
2017-03-21 22:54:04 -0500

    built everything

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
 console.log(jQuery);
+
+function renderToDoList() {
+    $('.container').remove();
+    $('body').append('<div class="container" ></div>')
+    $('.container').append('<header></header>');
+    $('header').append('<form id="addTask" method="post"><input type="text" name="Make a New List" placeholder="new task"><input class="submit" type="submit" value="Add Task"></form>');
+    $('.container').append('<div class="toDoListContainer"></div>');
+    $('.toDoListContainer').append('<ul></ul>');
+
+    var settings = {
+        type: 'GET',
+        dataType: 'json',
+        url: 'http://tiny-za-server.herokuapp.com/collections/devonmoubry-todo',
+    }
+
+    $.ajax(settings).then(function(data, status, xhr) {
+        data.forEach(function(task, key, listObj, argument) {
+            $('ul').append('<li><input type="checkbox" id="' + task._id + '"><label for="' + task._id + '">' + task.task + '</label><a id="' + task._id + '" href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a></li>');
+        })
+
+        // $('input').on('change', function(event) {
+        //   console.log(event.currentTarget.labels[0]);
+        //   $('event.currentTarget.labels[0]').addClass('checked');
+        // })
+
+        $('a').on('click', function(event) {
+            var id = event.currentTarget.id;
+            var url = 'http://tiny-za-server.herokuapp.com/collections/devonmoubry-todo/' + id;
+            var deleteSetting = {
+                type: 'DELETE',
+                url: url
+            };
+
+
+            $.ajax(deleteSetting).then(function(data, status, xhr) {
+                renderToDoList();
+            })
+        });
+    });
+}
+
+renderToDoList();
+
+$('#addTask').submit(function(event) {
+    event.preventDefault();
+    var addTaskInput = event.currentTarget[0].value;
+    var settings = {
+        type: 'POST',
+        contentType: 'application/json',
+        url: 'http://tiny-za-server.herokuapp.com/collections/devonmoubry-todo',
+        data: JSON.stringify({
+            'task': addTaskInput
+        })
+    }
+
+    $.ajax(settings).then(function(data, status, xhr) {
+        renderToDoList();
+    })
+})
